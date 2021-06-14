8d04d673ebb536426b1c2178511287da0d1c9ae4
Hugo <hugoomdra@gmail.com>
2021-06-14 18:36:32 +0200

    update db connection

---
+++ database/migrations/2021_06_14_162811_create_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->id();
+            $table->double('luminosity');
+            $table->double('battery_level');
+            $table->double('pressure');
+            $table->double('temperature');
+            $table->string('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+}
